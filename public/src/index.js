cd01dfc0e2cb46b610f60e4e7dd3b7df2d3a7eca
michaelballos <mdb3091207@gmail.com>
2022-03-16 09:18:55 -0500

    Update Insert Element Functions
    Create a standard structure for each function. Expand paramaters from displayName and id to include classes, id selector, and insert direction for more append control.

--- public/src/index.js
+++ public/src/index.js
@@ -1,81 +1,109 @@
-document.getElementById('root').innerHTML = `
-<div id="navbarContainer">
-<nav id="navbar">
-  <a class="navbarLogo" href="#">Navbar</a>
-  <button class="nav-toggle">
-    <span class="bar-top"></span>
-    <span class="bar-mid"></span>
-    <span class="bar-bot"></span>
-  </button> 
-   <div id="navItemContainer">
-    <ul id="navLinkContainer" class="navLinkContainer">
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
- </nav>
- </div>
-`;
-
-
-
-
-
+//sets id body root
+const body = document.body;
+body.setAttribute('id', 'root');
 
 /**
- * append reg li item into nav ul container   
- * @param {*} displayName
- * @returns
- */  
-appendListItem = (displayName) => {
-// assign & define string to append
-  const navItem =`
+ * id
+ */
+
+div = (id, className, appendId, insert) => {
+  const div = `
+  <div id="${id}" class="${className}"></div>
+`;
+  const append = document.getElementById(`${appendId}`);
+  append.insertAdjacentHTML(`${insert}`, div);
+  return;
+};
+
+nav = (id, className, appendId, insert) => {
+  const nav = `
+    <nav id="${id}" class="${className}"></nav>
+`;
+  const append = document.getElementById(`${appendId}`);
+  append.insertAdjacentHTML(`${insert}`, nav);
+  return;
+};
+
+a = (id, className, appendId, insert) => {
+  const a = `
+  <a id="${id}" class="${className}" href="#">Navbar</a>
+  `;
+  const navbar = document.getElementById(`${appendId}`);
+  navbar.insertAdjacentHTML(`${insert}`, a);
+  return;
+};
+
+navToggleBtn = (id, className, appendId, insert) => {
+  const btn = `
+  <a id="navLogo" href="#">Navbar</a>
+  <button id="${id}" class="${className}">
+    <span id="barTop"></span>  
+    <span id="barMid"></span>  
+    <span id="barBot"></span>  
+  </button> 
+  `;
+  const append = document.getElementById(`${appendId}`);
+  append.insertAdjacentHTML(`${insert}`, btn);
+  return;
+};
+
+ul = (id, className, appendId, insert) => {
+  const ul = `
+  <ul id="${id}" class="${className}"></ul> 
+  `;
+  const append = document.getElementById(`${appendId}`);
+  append.insertAdjacentHTML(`${insert}`, ul);
+  return;
+};
+
+li = (displayName) => {
+  const navItem = `
     <li class="navItem" href="#">
       <a class="navLink">
         ${displayName}
       </a>
     </li>
   `;
-
-const navLinkContainer = document.getElementById('navLinkContainer');  
-navLinkContainer.insertAdjacentHTML('beforeend', navItem);
-return;
+  const navLinkContainer = document.getElementById('navLinkContainer');
+  navLinkContainer.insertAdjacentHTML('beforeend', navItem);
+  return;
 };
 
-
-
-/**
- * append dropdown li w/ 3 sub items into ul container 
- * @param {*} dropdownTitle
- * @param {*} dropdown2
- * @param {*} dropdown3
- * @param {*} dropdown4
- * @returns
- */
-navbarDropdown = (dropdownLinkDisplay, dropdown1, dropdown2, dropdown3) => {
-// assign & define dropdown li string to append
+liDropdown = (liName, liSub1, liSub2, liSub3) => {
   const navDropdown = `
-  <li id="dropdownListContainer">
-    <a id="dropdownLink" href="#"> ${dropdownLinkDisplay} </a>
+    <li id="navlinkContainer">
+       <a id="dropdownLink" href="#"> ${liName} </a>
     <div id="dropdownContent">  
-       <a class="dropdownItemLink" href="#"> ${dropdown1} </a>
-       <a class="dropdownItemLink" href="#"> ${dropdown2} </a>
-      <div class="dropdownDivider"></div>
-       <a class="dropdownItemLink" href="#"> ${dropdown3} </a>
-    </div>
+       <a class="dropdownItemLink" href="#"> ${liSub1} </a>
+       <a class="dropdownItemLink" href="#"> ${liSub2} </a>
+    <div class="dropdownDivider"></div>
+       <a class="dropdownItemLink" href="#"> ${liSub3} </a>
+    </li>
   `;
-
-const dropdownLinkContainer = document.getElementById('navLinkContainer');
-dropdownLinkContainer.insertAdjacentHTML('beforeend', navDropdown);
-return;
+  const append = document.getElementById('navLinkContainer');
+  append.insertAdjacentHTML('beforeend', navDropdown);
+  return;
 };
 
+searchBar = (appendId, insert) => {
+  const search = `
+    <form id="searchBar">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  `;
+  const append = document.getElementById(`${appendId}`);
+  append.insertAdjacentHTML(`${insert}`, search);
+  return;
+};
 
-
-const homeLinkItem = appendListItem('Home');
-const linkListItem = appendListItem('Link');
-const dropdownListItem = navbarDropdown('Dropdown', 'Action', 'Another action', 'Something else here');
-const disabledListItem = appendListItem('Disabled');
-   
+div('navbarContainer', 'div', 'root', 'afterbegin');
+nav('navbar', 'nav', 'navbarContainer', 'afterbegin');
+navToggleBtn('navToggle', 'btn', 'navbar', 'afterbegin');
+div('navDropdownCollapse', 'div', 'navToggle', 'afterend');
+ul('navLinkContainer', 'ul', 'navDropdownCollapse', 'afterend');
+searchBar('navLinkContainer', 'afterend');
+li('Home');
+li('Link');
+liDropdown('Dropdown', 'Action', 'Another action', 'Something else here');
+li('Disabled');
