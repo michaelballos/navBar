d5d1d5453f8d71d8260f69a67ab444354fcde7ee
michaelballos <mdb3091207@gmail.com>
2022-04-04 21:16:57 -0500

    UpDate

--- web dev/michaelballos.github.io/pages/index.tsx
+++ web dev/michaelballos.github.io/pages/index.tsx
@@ -1,33 +1,29 @@
-import ParallaxPlain from '../components/ParallaxPlain';
-import LastName from '../components/LastName';
-import FirstName from '../components/FirstName';
-import ParallaxTechVaporwave from '../components/ParallaxTechVaporwave';
-import ICanWrite from '../components/ICanWrite';
-import ContactInfo from '../components/ContactInfo';
+import ParallaxPlain from '../components/ParallaxPlain'
+import FirstName from '../components/FirstName'
+import ParallaxTechVaporwave from '../components/ParallaxTechVaporwave'
+import ICanWrite from '../components/ICanWrite'
+import ContactInfo from '../components/ContactInfo'
 
 const HomePage = () => {
   return (
-    <>
-    <div className={'container'}>
-      <ParallaxPlain  />
-      <div className={'info'}>
-        <FirstName />  
-        <ContactInfo /> 
+    <div className='container'>
+      <ParallaxPlain />
+      <div className='info'>
+        <FirstName />
+        <ContactInfo />
       </div>
-      <ParallaxPlain  />
-      
+      <ParallaxPlain />
+
       <ICanWrite />
       <div className={'laserContainer'}>
-      <div className={'laser'} />
+        <div className={'laser'} />
       </div>
-      <div className={'leftBorder'} />
-      <div className={'rightBorder'} />
-      <div className={'bottomBorder'} />
+      <div className='leftBorder' />
+      <div className='rightBorder' />
+      <div className='bottomBorder' />
       <ParallaxTechVaporwave />
     </div>
-    </>
-  );
+  )
 }
 
-
-export default HomePage;
+export default HomePage
